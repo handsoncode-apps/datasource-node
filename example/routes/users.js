c533984e27c9f9a88d58915bd8ddfa1c217bd2ec
Kamil Dobrzynski <kamil.dobrzynski@kleder.co>
2018-02-23 15:54:29 +0100

    clean up

--- example/routes/users.js
+++ example/routes/users.js
@@ -1,88 +1,82 @@
-"use strict"
-var express = require('express')
-var bodyParser = require('body-parser')
-var router = express.Router()
+"use strict";
+var express = require("express");
+var bodyParser = require("body-parser");
+var router = express.Router();
 
 // create application/json parser
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
 var data = [
-  { 
+  {
     key: 5,
-    values: ['2017', 10, 11, 11, 15, 15, 16]
+    values: ["2017", 10, 11, 11, 15, 15, 16]
   },
-  { 
+  {
     key: 7,
-    values: ['2018', 13, 11, 12, 14, 15, 16]
+    values: ["2018", 13, 11, 12, 14, 15, 16]
   },
-  { 
+  {
     key: 11,
-    values: ['2019', 10, 11, 13, 9, 15, 16]
+    values: ["2019", 10, 11, 13, 9, 15, 16]
   },
-  { 
+  {
     key: 13,
-    values: ['2020', 10, 11, 14, 12, 15, 16]
+    values: ["2020", 10, 11, 14, 12, 15, 16]
   },
-  { 
+  {
     key: 15,
-    values: ['2020', 10, 11, 14, 12, 15, 16]
+    values: ["2020", 10, 11, 14, 12, 15, 16]
   }
 ];
-var colNames = ['year', 'Tesla', 'Nissan', 'Toyota', 'Honda', 'Mazda', 'Ford']
-var colOrder = [0,1,2,3,4,5,6]
-var dataAtBeginning = data
+var colNames = ["year", "Tesla", "Nissan", "Toyota", "Honda", "Mazda", "Ford"];
+var colOrder = [0, 1, 2, 3, 4, 5, 6];
+var dataAtBeginning = data;
 
 /**
-   * @param {{e.RequestHandler}} jsonParser
-   * @param {{changes:[{row:number,column:number,newValue:string}}], source:String}} req.body
-   */
-router.post('/afterchange', jsonParser, function (req, res, next) {
-  
-  
-  for (var i = 0; i < req.body.changes.length; i++){
-    var change = req.body.changes[i]
+ * @param {{e.RequestHandler}} jsonParser
+ * @param {{changes:[{row:number,column:number,newValue:string}}], source:String}} req.body
+ */
+router.post("/afterchange", jsonParser, function(req, res, next) {
+  for (var i = 0; i < req.body.changes.length; i++) {
+    var change = req.body.changes[i];
     data[change.row].values[change.column] = change.newValue;
-    console.log(change)
+    console.log(change);
   }
-  res.json({ 'data': 'ok' })
+  res.json({ data: "ok" });
+});
 
-  
-  
-})
-
-router.post('/aftercreaterow', jsonParser, function (req, res, next) {
-  var createRow = req.body
+router.post("/aftercreaterow", jsonParser, function(req, res, next) {
+  var createRow = req.body;
   for (var i = 0; i < createRow.amount; i++) {
-    data.splice(createRow.index, 0, { key: '', values: [] })
+    data.splice(createRow.index, 0, { key: "", values: [] });
   }
-})
+});
 
-router.post('/aftercreatecol', jsonParser, function (req, res, next) {
-  var createCol = req.body
-  colNames.splice(createCol.index, 0, '')
+router.post("/aftercreatecol", jsonParser, function(req, res, next) {
+  var createCol = req.body;
+  colNames.splice(createCol.index, 0, "");
   for (var i = 0; i < data.length; i++) {
-    data[i].values.splice([createCol.index], 0, '')
+    data[i].values.splice([createCol.index], 0, "");
   }
-})
+});
 
-router.get('/data', function (req, res, next) {
-  res.json({data: data, columns: colNames, colOrder: colOrder})
+router.get("/data", function(req, res, next) {
+  res.json({ data: data, columns: colNames, colOrder: colOrder });
+});
 
-})
-
-router.post('/aftercolumnsort', jsonParser, function (req, res, next) {
-  var tmp = req.body
+router.post("/aftercolumnsort", jsonParser, function(req, res, next) {
+  var tmp = req.body;
   var tempCol = [];
   var indexes = [];
   for (var i = 0; i < data.length; i++) {
-    tempCol.push(data[i].values[tmp.column])
+    tempCol.push(data[i].values[tmp.column]);
   }
   if (tmp.order) {
     var tempColIndexes = [];
     for (var i in tempCol) {
       tempColIndexes.push([tempCol[i], i]);
     }
-    tempColIndexes.sort(function (left, right) {
+    tempColIndexes.sort(function(left, right) {
       return left[0] < right[0] ? -1 : 1;
     });
     var temp = [];
@@ -95,7 +89,7 @@ router.post('/aftercolumnsort', jsonParser, function (req, res, next) {
     for (var i in tempCol) {
       tempColIndexes.push([tempCol[i], i]);
     }
-    tempColIndexes.sort(function (left, right) {
+    tempColIndexes.sort(function(left, right) {
       return left[0] < right[0] ? -1 : 1;
     });
     var temp = [];
@@ -103,35 +97,39 @@ router.post('/aftercolumnsort', jsonParser, function (req, res, next) {
       temp.push(tempColIndexes[j][0]);
       indexes.push(tempColIndexes[j][1]);
     }
-    indexes.reverse()
+    indexes.reverse();
   }
-  var sortedData = []
+  var sortedData = [];
   for (var i = 0; i < indexes.length; i++) {
-    sortedData.push({ key: data[indexes[i]].key, values: data[indexes[i]].values })
+    sortedData.push({
+      key: data[indexes[i]].key,
+      values: data[indexes[i]].values
+    });
   }
-  data = sortedData
+  data = sortedData;
   if (tmp.order == undefined) {
-    data = dataAtBeginning
+    data = dataAtBeginning;
   }
-  res.json({ 'data': data })
-})
+  res.json({ data: data });
+});
 
-router.post('/aftercolumnmove', jsonParser, function (req, res, next) {
-  var colMoved = req.body
+router.post("/aftercolumnmove", jsonParser, function(req, res, next) {
+  var colMoved = req.body;
 
-  var columns = colMoved.columns
-  var position = colMoved.target
+  var columns = colMoved.columns;
+  var position = colMoved.target;
 
-  var begin= colOrder.slice(0, position).filter(x => columns.indexOf(x) === -1 )
-  var end = colOrder.slice(position).filter(x => columns.indexOf(x) === -1)
-  
-  colOrder = begin
-  colOrder = colOrder.concat(columns)
-  colOrder = colOrder.concat(end)
-  
-  res.json({'data': colOrder})
-  
-})
+  var begin = colOrder
+    .slice(0, position)
+    .filter(x => columns.indexOf(x) === -1);
+  var end = colOrder.slice(position).filter(x => columns.indexOf(x) === -1);
+
+  colOrder = begin;
+  colOrder = colOrder.concat(columns);
+  colOrder = colOrder.concat(end);
+
+  res.json({ data: colOrder });
+});
 
 //TODO onDestroy => dataAtBeginning = data or smth like this
-module.exports = router
+module.exports = router;
