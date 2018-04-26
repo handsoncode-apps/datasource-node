f794f6b4309dd3fa33222cf919588cd4a49106a8
Kamil Dobrzynski <kamil.dobrzynski@kleder.co>
2018-04-26 11:51:27 +0200

    creating layout.pug in node generator

--- bin/index.js
+++ bin/index.js
@@ -45,6 +45,11 @@ let generate = (name) => {
         fs.mkdirSync(target);
       }
 
+      if (!fs.existsSync(target + '/layout.pug')) {
+        fs.createReadStream(path.format({dir: source, base: 'layout.pug'}))
+          .pipe(fs.createWriteStream(path.format({dir: target, base: 'layout.pug'})));
+      }
+
       fs.createReadStream(path.format({dir: source, base: 'basic.pug'}))
         .pipe(replace(/basic/g, name))
         .pipe(fs.createWriteStream(path.format({dir: target, base: `${name}.pug`})));
